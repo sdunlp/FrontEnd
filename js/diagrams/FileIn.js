ffbaf7bb76390f52b31abbc7f4c63eb413c596b4
jenniferxu1996 <675531471@qq.com>
2017-04-13 16:36:57 +0800

    a

--- js/diagrams/FileIn.js
+++ js/diagrams/FileIn.js
@@ -2,8 +2,24 @@
 $(document).ready(function() {
 	$("#input-6").fileinput({
 		showUpload: false,
-			maxFileCount: 10,
+			maxFileCount: 2000,
 			mainClass: "input-group-lg"
 						});
-					});
-			
+});
+
+    function upload() {
+        $('#fileinput').ajaxSubmit({
+            url: 'http://localhost/nlp_api/public/upload',
+            xhrFields: {
+                withCredentials: true
+            },
+            success:function (data) {  //data数组，里面保存对象，有两个属性，name表示文件名和message错误信息，如果数组有值，把这些错误的文件名都显示出来
+                
+            },
+            error:function (request,status) {
+                
+            }
+        });
+    }   
+	
+
